f8bf8814ab7df439f860c2cf06607a6e69e9a4dd
Zain-Yousuf <93894509+Zain-Yousuf@users.noreply.github.com>
2021-12-05 11:37:19 +0500

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+
+char str1[12] = "hello";
+char str2[12] = "world";
+char str3[12];
+int len;
+
+strcpy(str3,str1);
+printf("strcpy(str1,str3) : %s\n",str3);
+
+strcat(str1,str2);
+printf("strcat(str1,str2) : %s\n",str1);
+
+
+len = strlen(str1);
+printf("strlen(str1) :  %d\n",len);
+
+   return 0;
+
+} 
